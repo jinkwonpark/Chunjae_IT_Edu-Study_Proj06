f8546a0182b74203ced085c1b74dceacd951b050
jinkwonpark <jk3473@naver.com>
2023-11-30 00:14:44 +0900

    Thymeleaf 공부

---
+++ src/main/java/com/pro06/domain/Board.java
@@ -0,0 +1,26 @@
+package com.pro06.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Board extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    @Column(length = 500, nullable = false) // 칼럼의 길이와 null 허용여부
+    private String title;
+
+    @Column(length = 2000, nullable = false)
+    private String content;
+
+    @Column(length = 50, nullable = false)
+    private String writer;
+}
