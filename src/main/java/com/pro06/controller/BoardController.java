f667fe3a80ba786d6a8537ca53d7a4c3d823f1cd
jinkwonpark <jk3473@naver.com>
2023-12-07 17:34:45 +0900

    boardregister view 작성중

--- src/main/java/com/pro06/controller/BoardController.java
+++ src/main/java/com/pro06/controller/BoardController.java
@@ -4,12 +4,16 @@ import com.pro06.dto.BoardDTO;
 import com.pro06.dto.PageRequestDTO;
 import com.pro06.dto.PageResponseDTO;
 import com.pro06.service.BoardService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/board")
@@ -19,6 +23,7 @@ public class BoardController {
 
     private final BoardService boardService;
 
+    // 게시판 목록
     @GetMapping("/list")
     public void list(PageRequestDTO pageRequestDTO, Model model){
 
@@ -27,5 +32,31 @@ public class BoardController {
         log.info(responseDTO);
 
         model.addAttribute("responseDTO", responseDTO);
+    }
+
+    // 게시판 등록
+    @GetMapping("/register")
+    public void registerGET(){
+
+    }
+
+    @PostMapping("/register")
+    public String registerPost(@Valid BoardDTO boardDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes){
+
+        log.info("board POST register.......");
+
+        if(bindingResult.hasErrors()) {
+            log.info("has errors.......");
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            return "redirect:/board/register";
+        }
+
+        log.info(boardDTO);
+
+        Long bno = boardService.register(boardDTO);
+
+        redirectAttributes.addFlashAttribute("result", bno);
+
+        return "redirect:/board/list";
     }
 }
